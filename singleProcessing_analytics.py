6c9c3b225f155fd0f0b15d663308a9cd9f2af112
zephyrxvxx7 <zephyrxvxx7@gmail.com>
2018-05-08 14:41:55 +0800

    Add copy feature.

--- singleProcessing_analytics.py
+++ singleProcessing_analytics.py
@@ -11,18 +11,19 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
 
-    #N_list = [300000]
     for N in N_list:
-        randint_list = [randint(0, 2 ** 31 - 1) for _ in range(N)]
+        randint_list = [[randint(0, 2 ** 31 - 1)
+                         for _ in range(N)] for _ in range(ROUND)]
 
-        bubble_list = copy(randint_list)
-        insertion_list = copy(randint_list)
-        selection_list = copy(randint_list)
-        quick_list = copy(randint_list)
-        heap_list = copy(randint_list)
+        for i in range(ROUND):
+            bubble_list = copy(randint_list[i])
+            insertion_list = copy(randint_list[i])
+            selection_list = copy(randint_list[i])
+            quick_list = copy(randint_list[i])
+            heap_list = copy(randint_list[i])
 
-        sorts.bubble_sort(bubble_list)
-        sorts.insertion_sort(insertion_list)
-        sorts.selection_sort(selection_list)
-        sorts.quick_sort(quick_list)
-        sorts.heap_sort(heap_list)
+            sorts.bubble_sort(bubble_list)
+            sorts.insertion_sort(insertion_list)
+            sorts.selection_sort(selection_list)
+            sorts.quick_sort(quick_list)
+            sorts.heap_sort(heap_list)
