54acb947b9f69be4265c3110dbcab2992480dcf8
zephyrxvxx7 <zephyrxvxx7@gmail.com>
2018-05-11 03:14:20 +0800

    optimization parallel operation.

--- sorts.py
+++ sorts.py
@@ -2,33 +2,40 @@ import logging
 from time import clock
 from random import randint
 from functools import wraps
+from copy import deepcopy
 
 
 # decorator
 def time_analytics(func):
     @wraps(func)
     def my_wrap(*args, **kwargs):
-        logging.info('Starting {0}(N = {1})'.format(func.__name__, len(*args)))
+        logging.info('Starting {:<15}(N = {:6d})'.format(
+            func.__name__, len(*args)))
         t0 = clock()
         func(*args, **kwargs)
         use_time = clock() - t0
-        logging.info('Ending   {0}(N = {1}) in {2} sec'.format(
+        logging.info('Ending   {:<15}(N = {:6d}) in {} sec'.format(
             func.__name__, len(*args), use_time))
         return use_time
     return my_wrap
 
 
 @time_analytics
-def bubble_sort(list_):
+def bubble_sort(main_list):
+
+    list_ = deepcopy(main_list)
+
     for i in range(0, len(list_) - 1):  # 有n-1回合(n為數字個數)
         for j in range(0, len(list_) - 1 - i):  # 每回合進行比較的範圍
             if list_[j] > list_[j + 1]:  # 是否需交換
                 list_[j], list_[j + 1] = list_[j + 1], list_[j]
-    return list_
 
 
 @time_analytics
-def insertion_sort(list_):  # in-place
+def insertion_sort(main_list):  # in-place
+
+    list_ = deepcopy(main_list)
+
     for i in range(1, len(list_)):  # 第一個元素固定，從第二個開始
         temp = list_[i]
         j = i - 1  # 固定元素的前一個數字
@@ -36,11 +43,13 @@ def insertion_sort(list_):  # in-place
             list_[j + 1] = list_[j]  # 值向右
             j = j - 1
         list_[j + 1] = temp
-    return list_
 
 
 @time_analytics
-def selection_sort(list_):  # 由小到大 in-place
+def selection_sort(main_list):  # 由小到大 in-place
+
+    list_ = deepcopy(main_list)
+
     for i in range(0, len(list_) - 1):
         min_index = i
         for j in range(i + 1, len(list_)):
@@ -50,12 +59,12 @@ def selection_sort(list_):  # 由小到大 in-place
         if (min_index != i):
             list_[min_index], list_[i] = list_[i], list_[min_index]
 
-    return list_
-
 
 @time_analytics
-def quick_sort(list_):
+def quick_sort(main_list):
     """Source, https://en.wikipedia.org/wiki/Quicksort"""
+
+    list_ = deepcopy(main_list)
 
     def _quicksort(list_, begin, end):
         # must run partition on sections with 2 elements or more
@@ -77,12 +86,13 @@ def quick_sort(list_):
             begin += 1
             end -= 1
     _quicksort(list_, 0, len(list_) - 1)
-    return list_
 
 
 @time_analytics
-def heap_sort(list_):
+def heap_sort(main_list):
     """Source: https://en.wikipedia.org/wiki/Heapsort"""
+
+    list_ = deepcopy(main_list)
 
     def heapify(begin, end):
         """最大堆調整"""
@@ -107,7 +117,6 @@ def heap_sort(list_):
     for end in range(len(list_) - 1, 0, -1):
         list_[0], list_[end] = list_[end], list_[0]
         heapify(0, end - 1)
-    return list_
 
 
 if __name__ == '__main__':
