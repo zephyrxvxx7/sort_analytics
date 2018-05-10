008e8ede703e836c3711403ebaff70cf9d5075d2
zephyrxvxx7 <zephyrxvxx7@gmail.com>
2018-05-11 03:16:00 +0800

    Improve some efficacy.

--- multiProcessing_analytics.py
+++ multiProcessing_analytics.py
@@ -2,33 +2,66 @@ from multiprocessing import cpu_count, Pool
 from multiprocessing.pool import ThreadPool
 from random import randint
 import logging
-from copy import copy
+from copy import copy, deepcopy
+from time import clock
 
 import sorts
+from result_output import draw_figure
 
 if __name__ == '__main__':
+    t0 = clock()
 
-    N_list = [50000]
-    ROUND = 1
+    N_list = [50000, 100000, 150000]
+    ROUND = 5
 
     logging.basicConfig(level=logging.INFO)
-    pool = Pool(processes=4)
+    pool = Pool(processes=8)
 
+    results_list = []
     for N in N_list:
         randint_list = [[randint(0, 2 ** 31 - 1)
                          for _ in range(N)] for _ in range(ROUND)]
 
         for i in range(ROUND):
-            bubble_list = copy(randint_list[i])
-            insertion_list = copy(randint_list[i])
-            selection_list = copy(randint_list[i])
-            quick_list = copy(randint_list[i])
-            heap_list = copy(randint_list[i])
-            pool.apply_async(sorts.bubble_sort, (bubble_list, ))
-            pool.apply_async(sorts.insertion_sort, (insertion_list, ))
-            pool.apply_async(sorts.selection_sort, (selection_list, ))
-            pool.apply_async(sorts.quick_sort, (quick_list, ))
-            pool.apply_async(sorts.heap_sort, (heap_list, ))
+            results_list.append(pool.apply_async(
+                sorts.bubble_sort, (randint_list[i], )))
+            results_list.append(pool.apply_async(
+                sorts.insertion_sort, (randint_list[i], )))
+            results_list.append(pool.apply_async(
+                sorts.selection_sort, (randint_list[i], )))
+            results_list.append(pool.apply_async(
+                sorts.quick_sort, (randint_list[i], )))
+            results_list.append(pool.apply_async(
+                sorts.heap_sort, (randint_list[i], )))
 
-        pool.close()
-        pool.join()
+    pool.close()
+    pool.join()
+
+    use_time = clock() - t0
+    print('Total used time: {}'.format(use_time))
+
+    N_list_dict = {N: [] for N in N_list}
+
+    results_dict = {'Bubble Sort': deepcopy(N_list_dict),
+                    'Insertion Sort': deepcopy(N_list_dict),
+                    'Selection Sort': deepcopy(N_list_dict),
+                    'Quick Sort': deepcopy(N_list_dict),
+                    'Heap Sort': deepcopy(N_list_dict)
+                    }
+    count = 0
+    for N in N_list:
+        for i in range(ROUND):
+            results_dict['Bubble Sort'][N].append(
+                results_list[count].get())
+
+            results_dict['Insertion Sort'][N].append(
+                results_list[count + 1].get())
+            results_dict['Selection Sort'][N].append(
+                results_list[count + 2].get())
+            results_dict['Quick Sort'][N].append(
+                results_list[count + 3].get())
+            results_dict['Heap Sort'][N].append(
+                results_list[count + 4].get())
+            count = count + 5
+
+    draw_figure(results_dict)
